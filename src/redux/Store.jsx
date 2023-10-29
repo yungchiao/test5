bec1d78d7b811ab7b304c8930df52035919d7908
jolina <jolina4526@gmail.com>
2023-10-29 23:20:57 +0800

    add useslice

--- src/redux/Store.jsx
+++ src/redux/Store.jsx
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxJs/toolkit";
 import userReducer from "../reducers/User";
+// import userSlice from "./redux/userSlice";
 
 const store = configureStore({
   reducer: {
